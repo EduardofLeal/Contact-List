d603a9e8b3d2997a7f4edc9f07e47d735f9cad59
Eduardo Fagundes Leal <eduardofagundesleal@gmail.com>
2022-01-07 15:21:44 -0300

    Get photo from library and model cell
    Implementation of the appereance of the list and of the method of choosing photo from library.

--- app/src/main/java/com/example/teste/MainActivity.java
+++ app/src/main/java/com/example/teste/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
     public void navegaTela(View view){
         Intent intent = new Intent(MainActivity.this, SegundaTela.class);
--- app/src/main/java/com/example/teste/SegundaTela.java
+++ app/src/main/java/com/example/teste/SegundaTela.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SegundaTela extends AppCompatActivity {
 
-    ImageButton imageButton;
+    EditText txtNome, txtNumero, txtEndereco, txtEmail;
+    ImageView imgContato;
+    ListView listaDeContatos;
+    TextView txtTitulo;
+    Button btnAdicionar;
+    Uri imagemUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_tela);
 
+        txtNome = (EditText) findViewById(R.id.txtNome);
+        txtNumero = (EditText) findViewById(R.id.txtCell);
+        txtEndereco = (EditText) findViewById(R.id.txtEndereco);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        listaDeContatos = (ListView) findViewById(R.id.listaDeContatos);
+        imgContato = (ImageView) findViewById(R.id.imgFoto);
+
+        btnAdicionar = (Button) findViewById(R.id.btnSalva);
 
     }
     public void mudaFoto(View view){
@@ -26,7 +46,7 @@ public class SegundaTela extends AppCompatActivity {
         builder.setPositiveButton("Library", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                clicaCarregarImagem();
             }
         });
         builder.setNegativeButton("Camera", new DialogInterface.OnClickListener() {
@@ -36,5 +56,20 @@ public class SegundaTela extends AppCompatActivity {
             }
         });
         builder.create().show();
+    }
+    public void clicaCarregarImagem(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a photo for this contact"), 1);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (resultCode == 1) {
+                imagemUri = data.getData();
+                imgContato.setImageURI(data.getData());
+            }
+        }
     }
 }
